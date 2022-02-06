1c607a6a41979c77cfb79f2731f4f93e20a63c78
吴康俊 <alexwu@wukangjuns-MacBook-Pro.local>
2022-02-07 06:27:15 +0800

    todolist done

--- react-staging/03_src_todoList案例/components/Header/index.jsx
+++ react-staging/03_src_todoList案例/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import './index.css';
 
@@ -11,6 +12,12 @@ export default class Header extends Component {
     );
   }
 
+  //对接收的props进行：类型、必要性的限制
+  static propTypes = {
+    addTodo: PropTypes.func.isRequired
+  }
+
+  //键盘事件的回调
   handleKeyUp = (event) => {
     const {addTodo} = this.props;
     //解析赋值获取keyCode, target
